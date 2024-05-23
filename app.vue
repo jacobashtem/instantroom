1a129e610d103c7a67fbbfd64d277c8a2da3b9ed
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-05-23 10:18:12 +0200

    nuxt ui

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="text-2xl">
-    Test
+  <div class="text-5xl">
+    test
+    <UChip>
+    <UButton icon="i-heroicons-inbox" color="gray" />
+  </UChip>
   </div>
 </template>
