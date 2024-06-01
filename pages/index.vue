e218e54c6f6e6530f44d13633b46e862a925bda1
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-06-01 15:09:15 +0200

    comparisonSlider component

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-    <section class="flex items-center justify-between mb-10">
-      <h1 class="text-4xl font-extrabold">
+    <section class="flex items-center flex-col lg:flex-row justify-between mb-10">
+      <h1 class="text-4xl font-extrabold mb-6 lg:mb-0">
         Redesign your room in seconds
       </h1>
-      <div>
 
+      <ComparisonSlider :images="images" />
+      <div>
       </div>
     </section>
   </template>
-  
+  <script setup>
+  import img1 from '@/public/examples/before-image-2.webp';
+  import img2 from '@/public/examples/after-image-2.webp';
+  const images = [img1, img2]
+  </script>
