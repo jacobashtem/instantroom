fd1109437a9665537ad59e2d2f183fc2776b1067
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-06-02 21:52:32 +0200

    themes implemented

--- server/api/custom-predictions.js
+++ server/api/custom-predictions.js
@@ -1,5 +1,4 @@
 import replicate from '~/server/services/replicate';
-import { serverSupabaseClient } from '#supabase/server'
 
 const WEBHOOK_HOST = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -13,33 +12,15 @@ export default defineEventHandler(async (event) => {
   }
 
   const body = await readBody(event);
-  const { image, theme, roomType, prompt } = body;
-  const supabase = await serverSupabaseClient(event)
-
-  // Pobranie prompta z Supabase
-  const { data, error } = await supabase
-    .from('prompts')
-    .select('room_types')
-    .eq('theme', theme)
-    .single();
-
-  if (error) {
-    event.res.statusCode = 500;
-    return { detail: error.message };
-  }
-
-  if (!data || !data.room_types[roomType]) {
-    event.res.statusCode = 400;
-    return { detail: "Invalid theme or room type." };
-  }
-
-  let finalPrompt = theme ? data.room_types[roomType].prompt : prompt;
-
+  const { image, prompt } = body;
   const options = {
+    // Interior design
     version: '76604baddc85b1b4616e1c6475eca080da339c8875bd4996705440484a6eac38',
+    // Image Generator
+    // 8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f
     input: {
       image: image,
-      prompt: finalPrompt,
+      prompt: prompt,
       guidance_scale: 15,
       negative_prompt: "lowres, watermark, banner, logo, watermark, contactinfo, text, deformed, blurry, blur, out of focus, out of frame, surreal, extra, ugly, upholstered walls, fabric walls, plush walls, mirror, mirrored, functional, realistic",
       prompt_strength: 0.8,
