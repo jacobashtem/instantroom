05d11011e1642ee43ce29ba23d8602c753da3fd5
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-07-04 19:36:25 +0200

    stripe test

--- pages/payments.vue
+++ pages/payments.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="bg-aquaBlue-500">
         <div class="mx-auto pt-40 pb-40 px-3 my-3 container max-w-6xl">
-            <div class="space-y-4">
+            <div class="mb-10">
                 <div class="space-y-4">
                     <span
                         class="rounded-xl uppercase bg-sunsetOrange-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
@@ -26,7 +26,7 @@
                         <div class="py-5 text-sunsetOrange-500 bg-white rounded-xl">
                             <h3>Pakiet podstawowy</h3>
                             <p class="text-5xl font-bold pb-0">
-                                19.90<span class="text-2xl">zł</span>
+                                17.90<span class="text-2xl">zł</span>
                             </p>
                             <span
                         class="rounded-xl uppercase bg-sunsetOrange-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
@@ -36,7 +36,35 @@
                         </div>
                         <div class="flex justify-center">
                             <div class="py-5 text-black rounded-b font-light text-xl flex flex-col items-start">
-                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>66 groszy za wizualizację!</p>
+                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>60 groszy za wizualizację!</p>
+                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>Another feature plan feature</p>
+                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>Yet another plan feature</p>
+                            </div>
+                        </div>
+                        <NuxtLink to="https://buy.stripe.com/test_00gaGPgs34Du65ycMM">
+                            <UButton variant="solid" class="mt-4 focus:shadow-outline focus:outline-nonetracking-wide font-semibold bg-sunsetOrange-500 hover:bg-sunsetOrange-700 text-gray-100 py-4 rounded-lg transition-all duration-300 ease-in-out text-lg px-4 flex justify-center mx-8">
+                                <UIcon width="24" height="24" name="mdi:cart-outline" dynamic /> Kup pakiet
+                            </UButton>
+                        </NuxtLink>
+                    </div>
+                </div>
+                <!-- standard plan -->
+                <div class="w-full p-4 md:w-1/2 lg:w-1/3">
+                    <div class="flex flex-col rounded-xl bg-white  text-black pb-4 shadow-2xl">
+                        <div class="py-5 text-sunsetOrange-500 bg-white rounded-xl">
+                            <h3>Pakiet podstawowy</h3>
+                            <p class="text-5xl font-bold pb-0">
+                                39.90<span class="text-2xl">zł</span>
+                            </p>
+                            <span
+                        class="rounded-xl uppercase bg-sunsetOrange-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
+                            80 tokenów
+                        </span>
+                            <p class="text-md uppercase"></p>
+                        </div>
+                        <div class="flex justify-center">
+                            <div class="py-5 text-black rounded-b font-light text-xl flex flex-col items-start">
+                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>50 groszy za wizualizację!</p>
                                 <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>Another feature plan feature</p>
                                 <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>Yet another plan feature</p>
                             </div>
@@ -46,42 +74,28 @@
                         </UButton>
                     </div>
                 </div>
-                <!-- standard plan -->
-                <div class="w-full p-4 md:w-1/2 lg:w-1/3">
-                    <div class="flex flex-col rounded-xl border-2 border-midnightBlue-500 bg-midnightBlue-500 pb-4">
-                        <div class="py-5 text-sunsetOrange-500 bg-white">
-                            <h3>Pakiet rozszerzony</h3>
-                            <p class="text-5xl font-bold">
-                                39.90<span class="text-2xl">zł</span>
-                            </p>
-                                80 tokenów
-                        </div>
-                        <div class="py-5 bg-midnightBlue-500 text-white rounded-b">
-                            <p>~ 50 gr za wizualizację!</p>
-                            <p>Another feature plan feature</p>
-                            <p>Yet another plan feature</p>
-                        </div>
-                        <UButton variant="solid" class="mt-4 focus:shadow-outline focus:outline-nonetracking-wide font-semibold bg-sunsetOrange-500 hover:bg-sunsetOrange-700 text-gray-100 py-4 rounded-xl transition-all duration-300 ease-in-out text-lg px-4 flex justify-center mx-8">
-                            <UIcon width="24" height="24" name="mdi:cart-outline" dynamic /> Kup pakiet
-                        </UButton>
-                    </div>
-                </div>
                 <!-- Luxury plan -->
                 <div class="w-full p-4 md:w-1/2 lg:w-1/3">
-                    <div class="flex flex-col rounded-xl border-2 border-midnightBlue-500 bg-midnightBlue-500 pb-4">
-                        <div class="py-5 text-sunsetOrange-500 bg-white">
-                            <h3>Pakiet luksusowy</h3>
-                            <p class="text-5xl font-bold">
+                    <div class="flex flex-col rounded-xl bg-white  text-black pb-4 shadow-2xl">
+                        <div class="py-5 text-sunsetOrange-500 bg-white rounded-xl">
+                            <h3>Pakiet podstawowy</h3>
+                            <p class="text-5xl font-bold pb-0">
                                 79.90<span class="text-2xl">zł</span>
                             </p>
-                                200 tokenów
+                            <span
+                        class="rounded-xl uppercase bg-sunsetOrange-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
+                            200 tokenów
+                        </span>
+                            <p class="text-md uppercase"></p>
                         </div>
-                        <div class="py-5 bg-midnightBlue-500 text-white rounded-b">
-                            <p>~ 40 gr za wizualiację!</p>
-                            <p>Another feature plan feature</p>
-                            <p>Yet another plan feature</p>
+                        <div class="flex justify-center">
+                            <div class="py-5 text-black rounded-b font-light text-xl flex flex-col items-start">
+                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>40 groszy za wizualizację!</p>
+                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>Another feature plan feature</p>
+                                <p><UIcon  width="24" height="24" class="text-aquaBlue-500 mr-4" dynamic name="mdi:tick-all"/>Yet another plan feature</p>
+                            </div>
                         </div>
-                        <UButton variant="solid" class="mt-4 focus:shadow-outline focus:outline-nonetracking-wide font-semibold bg-sunsetOrange-500 hover:bg-sunsetOrange-700 text-gray-100 py-4 rounded-xl transition-all duration-300 ease-in-out text-lg px-4 flex justify-center mx-8">
+                        <UButton variant="solid" class="mt-4 focus:shadow-outline focus:outline-nonetracking-wide font-semibold bg-sunsetOrange-500 hover:bg-sunsetOrange-700 text-gray-100 py-4 rounded-lg transition-all duration-300 ease-in-out text-lg px-4 flex justify-center mx-8">
                             <UIcon width="24" height="24" name="mdi:cart-outline" dynamic /> Kup pakiet
                         </UButton>
                     </div>
