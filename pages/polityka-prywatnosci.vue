06edc4d041b610db69709244f4f2c9b7f03125f3
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-07-18 20:06:09 +0200

    document pages, rodo, links to footer

---
+++ pages/polityka-prywatnosci.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="bg-aquaBlue-500 h-64">
+        <div class="mx-auto pt-40 pb-40 px-3 my-3 container max-w-6xl">
+            <div class="mb-10">
+                <div class="space-y-4">
+
+                    <h1 class="text-5xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
+                        <span class="sm:text-6xl">
+                        </span> Polityka  <span
+                            class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sunsetOrange-500 to-sunsetOrange-800">prywatności</span>
+                        <br>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <article class="mx-auto container max-w-6xl mt-10">
+        Polityka prywatności
+    </article>
+</template>
