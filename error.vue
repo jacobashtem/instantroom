7166e9ff68c1e413b5c32c32adc315fcafaf7162
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-08-07 16:52:38 +0200

    404 page finished

---
+++ error.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <AppHeader />
+      <main>
+        <div class="flex items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat"
+            style="background-image: url('404.webp')">
+            <div class="max-w-md mx-auto text-center bg-white bg-opacity-90 p-8 rounded-lg shadow-lg">
+                <h1 class="text-4xl font-bold text-gray-800 mb-6">Oops! Pokój nie został znaleziony!</h1>
+                <p class="text-lg text-gray-600 mb-8">Wygląda na to, że zgubiłeś/aś się w labiryncie pomieszczeń. Spokojnie, pomożemy Ci trafić do wymarzonego pokoju.</p>
+                <NuxtLink to="/"
+                class="bg-sunsetOrange-500 inline-block bg-customPrimary-600 text-white font-semibold px-6 py-3 rounded-md hover:bg-customPrimary-700 transition-colors duration-300">
+                Wróć na stronę główną
+            </NuxtLink>
+            </div>
+            </div>
+      </main>
+      <AppFooter />
+    </div>
+  </template>
+  
+  
+  
+  <script setup>
+  defineProps({ error: Object })
+  </script>
