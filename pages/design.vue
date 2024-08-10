ba58e47fa2895281f3fb5b80a73eb72e78d44fd4
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-08-10 20:28:25 +0200

    bugfix uploaded img

--- pages/design.vue
+++ pages/design.vue
@@ -128,8 +128,8 @@ const { inflectToken, inflectVisualization } = useInflection();
 const { toastSuccess, toastError } = useAppToast();
 const { data: userData, error: fetchError } = await supabase.auth.getUser();
 const images =  [];
-images.value = userData?.user.user_metadata.imagesUploaded;
-userUploadedPhotos.value = userData?.user.user_metadata.imagesUploaded.slice().reverse();
+images.value = userData?.user?.user_metadata?.imagesUploaded || [];
+userUploadedPhotos.value = (userData?.user?.user_metadata?.imagesUploaded?.slice() || []).reverse();
 
 const themes = ref([]);
 const roomTypes = ref([
