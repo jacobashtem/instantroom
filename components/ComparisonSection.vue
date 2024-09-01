3acfad4c172e5647d8acac3ed445744babb1598f
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-09-01 22:08:59 +0200

    comparison cmp  finished

---
+++ components/ComparisonSection.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="about" class="w-full relative bg-white overflow-hidden mt-14 mb-24 lg:mt-24 lg:mb-32">
+    <div class="max-w-6xl mx-auto">
+        <div class="relative z-10  bg-white lg:max-w-2xl lg:w-full">
+            <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-24 text-white transform translate-x-1/2"
+                fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+                <polygon points="50,0 100,0 50,100 0,100"></polygon>
+            </svg>
+
+            <div class="pt-1"></div>
+
+            <div class="mt-10 mx-auto max-w-6xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <div class="sm:text-center lg:text-left">
+                    <h2 class="my-6 text-3xl font-bold tracking-tight text-black sm:text-4xl md:text-6xl">
+                        Do czego przyda Ci się <span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sunsetOrange-500 to-sunsetOrange-800">Instant Room?</span>
+                    </h2>
+
+                    <p class="text-xl lg:text-2xl mt-6 font-light">
+                        Instant Room to przede wszystkim <span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sunsetOrange-500 to-sunsetOrange-800">generator inspiracji</span> i pomysłów! Nasze wizualizacje ułatwią Twoją komunikację z ekipą remontową, czy projektantami wnętrz.<br/><br/>
+                        Nie musisz sobie 'wyobrażać' -  zobacz czy bardziej pasowałyby Ci <span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sunsetOrange-500 to-sunsetOrange-800"> panele</span>, czy może jednak <span class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sunsetOrange-500 to-sunsetOrange-800">glazura!</span> 
+                        Instant Room nie jest kompleksowym narzędziem do projektowania wnętrz,<span class="bg-sunsetOrange-500 font-semibold text-white leading-6"> nie zamierzamy zabrać pracy architektom :) </span> <br/><br/>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <NuxtImg class="h-72 w-full object-scale-down object-cover object-top sm:h-72 md:h-96 lg:w-full lg:h-full" src="/background-comparisonwebp.webp" alt=""/>
+    </div>
+</div>
+</template>
