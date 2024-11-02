ebcd6bc118d99af5b3a98f4b027d2462c2de4e23
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-11-02 09:39:20 +0100

    blog components

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -64,6 +64,11 @@ watch(tokens, (newTokens) => {
       </NuxtLink>
       <div v-if="!isMobile">
         <!-- Logowanie -->
+        <NuxtLink to="/blog" 
+          :class="{'text-aquaBlue-500 font-semibold': !isLoggedIn}"
+          class="text-sm mr-4">
+          Blog
+        </NuxtLink>
         <NuxtLink v-if="!isLoggedIn" to="/login" class="text-sm font-semibold text-aquaBlue-500">
           Logowanie / Rejestracja
         </NuxtLink>
